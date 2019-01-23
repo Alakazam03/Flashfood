7625261b0944a0d025ccf3ab6b5a561c08c76caf
vaibhav aggarwal <vaibhav.045@iiitb.org>
2019-01-24 00:05:46 +0530

    Update Flashbot.js

--- Flashbot.js
+++ Flashbot.js
@@ -405,9 +405,9 @@ function receivedPostback(event) {
     }
 
   function toTitleCase(str) {
-        return str.replace(/\w\S*/g, function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        });
+       /* return str.replace(/\w\S*///g, function(txt) {
+          //  return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        //});*/
     }
 
 
